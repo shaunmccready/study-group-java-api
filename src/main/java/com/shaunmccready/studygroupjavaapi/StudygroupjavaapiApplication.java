ceba8ef0fea034920d1d6bdca09a3fcbf855f51f
Shaun McCready <shaunwmccready@gmail.com>
2019-02-22 23:45:31 -0500

    Initial Commit of code and basic testing of authentication

---
+++ src/main/java/com/shaunmccready/studygroupjavaapi/StudygroupjavaapiApplication.java
@@ -0,0 +1,13 @@
+package com.shaunmccready.studygroupjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudygroupjavaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudygroupjavaapiApplication.class, args);
+	}
+
+}
